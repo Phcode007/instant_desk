2ee5ce859a30d61b8491627a375e98824725b6f4
Paulo Henrique Belarmino <149791777+Phcode007@users.noreply.github.com>
2026-08-01 03:19:21 +0000

    feature: Adicionar módulo de autenticação e implementar serviço de criptografia de senhas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './ticket/entities/ticket.entity';
+import { Category } from './category/entities/category.entity';
+import { AuthModule } from './auth/auth.module';
 import { TicketModule } from './ticket/ticket.module';
 import { CategoryModule } from './category/category.module';
 
@@ -13,11 +15,12 @@ import { CategoryModule } from './category/category.module';
       username: 'postgres',
       password: 'postgres123',
       database: 'db_instant_desk',
-      entities: [Ticket],
+      entities: [Ticket, Category],
       synchronize: true,
     }),
     TicketModule,
     CategoryModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
