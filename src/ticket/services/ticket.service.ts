46f076201ea1cdc55c178d3c2cece9f3440e0daa
Paulo Henrique Belarmino <149791777+Phcode007@users.noreply.github.com>
2026-08-03 07:57:30 +0000

    feature: Adicionar relações de Category, Priority e User nas buscas de Ticket

--- src/ticket/services/ticket.service.ts
+++ src/ticket/services/ticket.service.ts
@@ -19,16 +19,21 @@ export class TicketService {
   }
 
   async findById(id: number): Promise<Ticket> {
-    const ticket = await this.ticketRepository.findOne({ where: { id } });
+    const ticket = await this.ticketRepository.findOne({
+      where: { id },
+      relations: { category: true, priority: true, user: true },
+    });
 
     if (!ticket) {
       throw new HttpException('Ticket não encontrado', HttpStatus.NOT_FOUND);
     }
     return ticket;
   }
+
   async findByDescricao(descricao: string): Promise<Ticket[]> {
     return await this.ticketRepository.find({
       where: { descricao: ILike(`%${descricao}%`) },
+      relations: { category: true, priority: true, user: true },
     });
   }
 
