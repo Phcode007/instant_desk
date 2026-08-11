657126e585454732fb9cf008c5c276ae5b6b53d7
Phcode007 <paulobelarmino047@gmail.com>
2026-08-11 13:00:12 -0300

    feat: add better-sqlite3 dependency and update database type in user tests

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,19 +1,24 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { describe } from 'node:test';
 
 describe('Testes dos Módulos User e Auth (e2e)', () => {
+  let token: any;
+  let usuarioId: any;
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         TypeOrmModule.forRoot({
-          type: 'sqlite',
+          type: 'better-sqlite3',
           database: ':memory:',
           entities: [__dirname + './../src/**/entities/*.entity.ts'],
           synchronize: true,
@@ -30,5 +35,65 @@ describe('Testes dos Módulos User e Auth (e2e)', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  it('01 - Deve Cadastrar um novo Usuário', async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/users/cadastrar')
+      .send({
+        nome: 'Root',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+      })
+      .expect(201);
+
+    usuarioId = resposta.body.id;
+  });
+
+  it('02 - Não Deve Cadastrar um Usuário Duplicado', async () => {
+    await request(app.getHttpServer())
+      .post('/users/cadastrar')
+      .send({
+        nome: 'Root',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+      })
+      .expect(400);
+  });
+
+  it('03 - Deve Autenticar o Usuário (Login)', async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/users/logar')
+      .send({
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+      })
+      .expect(200);
+
+    token = resposta.body.token;
+  });
+
+  it('04 - Deve Listar todos #1 os Usuários', async () => {
+    return request(app.getHttpServer())
+      .get('/users/all')
+      .set('Authorization', `${token}`)
+      .send({})
+      .expect(200);
+  });
+
+  it('05 - Deve Atualizar um Usuário', async () => {
+    return request(app.getHttpServer())
+      .put('/users/atualizar')
+      .set('Authorization', `${token}`)
+      .send({
+        id: usuarioId,
+        nome: 'Root Atualizado',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+      })
+      .expect(200)
+      .then((resposta) => {
+        expect('Root Atualizado').toEqual(resposta.body.nome);
+      });
   });
 });
