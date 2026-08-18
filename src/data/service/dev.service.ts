8ddba246b81f804cdcda6ba726ecdb53bc41de0f
Phcode007 <paulobelarmino047@gmail.com>
2026-08-17 21:30:34 -0300

    fix: atualização do banco de dados no DevService.

--- src/data/service/dev.service.ts
+++ src/data/service/dev.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { Category } from 'src/category/entities/category.entity';
-import { Company } from 'src/company/entities/company.entity';
-import { Priority } from 'src/priority/entities/priority.entity';
-import { Ticket } from 'src/ticket/entities/ticket.entity';
-import { User } from 'src/user/entities/user.entity';
+import { Category } from '../../category/entities/category.entity';
+import { Company } from '../../company/entities/company.entity';
+import { Priority } from '../../priority/entities/priority.entity';
+import { Ticket } from '../../ticket/entities/ticket.entity';
+import { User } from '../../user/entities/user.entity';
 
 @Injectable()
 export class DevService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      type: 'mysql',
+      type: 'postgres',
       host: 'localhost',
       port: 5432,
       username: 'postgres',
